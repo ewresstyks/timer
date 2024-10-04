447bb7a63a2a8ed9d2c9004bff3e91a0e13808fb
margo <margo@lalala>
2024-10-04 09:08:28 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+let a = document.getElementById("divfirst");
+a.innerText="What is Lorem Ipsum?";
+a.style.color="white";
+a.style.background="gray";
+
+let b = document.getElementById("divtwo");
+b.innerText="Why do we use it?";
+b.style.color="white";
+b.style.background="gray";
+
+let c = document.getElementById("divthree");
+c.innerText="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
+c.style.color="white";
+c.style.background="blue";
+
+let d = document.getElementById("divfour");
+d.innerText="Where does it come from?";
+d.style.color="white";
+d.style.background="gray";
+
+let e = document.getElementById("divfive");
+e.innerText="Where can I get some?";
+e.style.color="white";
+e.style.background="gray";
